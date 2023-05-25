50c8716b88880384c6154da6d6811f5730bac1f5
sebastien-delphine <sebastien.delphine@carbon-it.com>
2023-05-25 15:53:26 +0200

    init project

---
+++ app.js
@@ -0,0 +1,11 @@
+import createError from 'http-errors';
+import express from 'express';
+import { router as indexRouter } from './routes/index.js';
+import { router as usersRouter } from './routes/users.js';
+
+var app = express();
+
+app.use('/', indexRouter);
+app.use('/users', usersRouter);
+
+export { app };
---
+++ server.js
@@ -0,0 +1,28 @@
+#!/usr/bin/env node
+
+/**
+ * Module dependencies.
+ */
+
+import { app } from './app.js';
+import http from 'http';
+
+/**
+ * Get port from environment and store in Express.
+ */
+const PORT = process.env.PORT || '3000'
+app.set('port', PORT);
+
+/**
+ * Create HTTP server.
+ */
+
+var server = http.createServer(app);
+
+/**
+ * Listen on provided port, on all network interfaces.
+ */
+
+server.listen(PORT);
+// server.on('listening', onListening);
+console.log('Listening on ' + PORT + "...");
